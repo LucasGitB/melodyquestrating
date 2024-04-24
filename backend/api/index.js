5273ce3d0d2491bb9354c1be6d147e895b366ca2
LucasGitB <lucasaguesse@gmail.com>
2024-04-24 22:44:39 +0200

    config for vercel deployement

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
-// api/index.js
-import app from '../server.js';
+// backend/api/index.js
+import { app } from './server.js'; // Importez app de manière appropriée
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
-// server.js
-
+// backend/api/server.js
 import express from 'express';
 import cors from 'cors';
 import { Sequelize } from 'sequelize';
 import User from './User.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-import sequelize from './database.js';
 
 // Middleware pour parser le JSON dans les requêtes
 app.use(express.json());
@@ -44,19 +40,4 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Not found' });
 });
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-
-    // Synchroniser les modèles avec la base de données
-    await sequelize.sync({ alter: true });
-
-    // Lancer le serveur Express
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+export { app }; // Exportez app de manière explicite
